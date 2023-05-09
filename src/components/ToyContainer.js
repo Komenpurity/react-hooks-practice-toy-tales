9a4b74a77ba9cb1e4824228354819a32da6f3727
Komenpurity <jepkorirpurity8@gmail.com>
2023-05-09 10:14:48 +0300

    added a get request

--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({toys}) {
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">
+      {toys.map((toy) => {
+        return <ToyCard name={toy.name} id = {toy.id} image = {toy.image} likes = {toy.likes}/>
+      })}
+    </div>
   );
 }
 
